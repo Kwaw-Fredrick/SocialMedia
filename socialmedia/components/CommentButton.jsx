38bfd13952f678e4f3f4f74ef4f2c5dd3916c3bf
kwaw-fredrick <kwawfredrick35@gmail.com>
2026-06-16 09:15:18 +0000

    Comment Button created

--- socialmedia/components/CommentButton.jsx
+++ socialmedia/components/CommentButton.jsx
@@ -1,8 +1,21 @@
+import { Icon } from '@iconify/react/dist/iconify.js'
+import { Button, Flex, Typography } from 'antd'
 import React from 'react'
 
 const CommentButton = ({comments}) => {
   return (
-    <div>CommentButton</div>
+   <Button>
+    <Flex gap={'0.5rem'} align= 'center'>
+        <Icon
+         icon="iconamoon: comment-dots-fill"
+         width={"21"}
+         color='grey'
+        />
+        <Typography.Text>
+            {comments > 0? `${comments} Comments` : "Comment" }
+        </Typography.Text>
+    </Flex>
+   </Button>
   )
 }
 
